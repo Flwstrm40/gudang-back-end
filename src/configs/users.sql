8e1388d2c2f62930eb0a3597fa3352b8cbead0ca
Muhammad Naufal Arkan <naufalarkan@students.undip.ac.id>
2024-01-09 15:13:04 +0700

    add displayName Field on users

--- src/configs/users.sql
+++ src/configs/users.sql
@@ -3,5 +3,6 @@ CREATE TABLE users (
   id INT PRIMARY KEY AUTO_INCREMENT,
   username VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL,
+  displayName VARCHAR(255),
   role VARCHAR(255) NOT NULL
 );
