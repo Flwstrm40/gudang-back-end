d384d3e1c794f13af584db5b42fd67410f137278
Muhammad Naufal Arkan <naufalarkan@students.undip.ac.id>
2024-01-16 09:33:34 +0700

    update sql

--- src/configs/in_histories.sql
+++ src/configs/in_histories.sql
@@ -1,8 +1,9 @@
 CREATE TABLE in_histories (
     id_history_masuk INT PRIMARY KEY AUTO_INCREMENT,
-    id_produk INT,
-    stok_masuk INT,
-    tanggal DATE,
-    jam TIME,
-    keterangan VARCHAR(255)
+    id_produk INT NOT NULL,
+    stok_masuk INT NOT NULL,
+    tanggal DATE NOT NULL,
+    jam TIME NOT NULL,
+    keterangan VARCHAR(255),
+    pj VARCHAR(50) NOT NULL
 );
