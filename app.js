1cf7a684403a12135e724da7d5d0c438b518f467
Muhammad Naufal Arkan <naufalarkan@students.undip.ac.id>
2024-01-21 23:04:30 +0700

    bismillah

--- app.js
+++ app.js
@@ -20,11 +20,6 @@ dotenv.config({ path: './.env' });
 
 const app = express();
 
-app.use(cors());
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
@@ -32,6 +27,12 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
+
+app.use(cors());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 
 
 // Routes for the user entity
