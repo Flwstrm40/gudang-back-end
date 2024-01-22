22846831eeb8cb57d35772a6b28f30d9dda811d8
Muhammad Naufal Arkan <naufalarkan@students.undip.ac.id>
2024-01-22 22:25:51 +0700

    Migrate db hosting to filess.io

--- src/db/db.js
+++ src/db/db.js
@@ -4,7 +4,12 @@ dotenv.config({ path: './.env' });
 
 // Buat koneksi MySQL dari URL
 const db = mysql.createConnection(process.env.MYSQL_URL);
-
+// const db = mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME
+// });
 db.connect((err) => {
     if (err) {
         console.error('Error connecting to MySQL:', err.message);
