35758aa4bfb4548d7a59ac2c22be89b78f669638
Muhammad Naufal Arkan <naufalarkan@students.undip.ac.id>
2024-02-06 19:24:32 +0700

    change host database to Tidb

--- src/db/db.js
+++ src/db/db.js
@@ -3,13 +3,13 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './.env' });
 
 // Buat koneksi MySQL dari URL
-// const db = mysql.createConnection(process.env.MYSQL_URL);
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
+const db = mysql.createConnection(process.env.MYSQL_URL);
+// const db = mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME
+// });
 db.connect((err) => {
     if (err) {
         console.error('Error connecting to MySQL:', err.message);
